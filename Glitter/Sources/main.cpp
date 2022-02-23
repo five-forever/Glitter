85a433be75f363538ab8c5ecc3894215eb382493
TalosGuo <talosguo@vesync.com.cn>
2022-02-23 19:03:58 +0800

    111

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -12,22 +12,34 @@
 
 const char *vertexShaderSource = "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
+    "out vec4 vertexColor;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   vertexColor = vec4(0.0f, 0.5f, 0.2f, 1.0f);"
     "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
+    "in vec4 vertexColor;\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
-    "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "FragColor = vertexColor;\n"
+    "}\0";
+
+const char *fragmentShaderSourceGreen = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "FragColor = vec4(0.5f, 0.5f, 0.2f, 1.0f);\n"
     "}\0";
 
 using namespace std;
 void framebuffer_size_callback(GLFWwindow* window,int width, int height);
 void processInput(GLFWwindow *window);
-void drawTriangle();
+void drawTriangle(GLFWwindow *window);
+void drawTwoTrians(GLFWwindow *window);
+unsigned int createProgram();
 
 int main() {
     // Load GLFW and Create a Window
@@ -37,7 +49,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     
-    GLFWwindow* window = glfwCreateWindow(1800, 1600, "learn", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "learn", NULL, NULL);
     if (window == NULL) {
         cout << "failed to load" << endl;
         glfwTerminate();
@@ -50,24 +62,11 @@ int main() {
         return -1;
     }
     
-    glViewport(0, 0, 1800, 1600);
+    glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
-    while (!glfwWindowShouldClose(window)) {
-        // 输入
-        processInput(window);
-        
-        // 渲染
-        drawTriangle();
-        // 状态设置
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        // 状态使用
-        glClear(GL_COLOR_BUFFER_BIT);
-        
-        // 检查并调用事件，交换缓冲
-        glfwPollEvents();
-        glfwSwapBuffers(window);
-    }
+    drawTriangle(window);
+    
     glfwTerminate();
     return 0;
 }
@@ -81,8 +80,8 @@ void processInput(GLFWwindow *window) {
         glfwSetWindowShouldClose(window, true);
     }
 }
-/// 画三角形
-void drawTriangle() {
+/// 创建着色器和程序
+unsigned int createProgram(const char *vertexShaderSource, const char *fragmentShaderSource) {
     // 创建着色器
     unsigned int vertexShader;
     vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -112,29 +111,105 @@ void drawTriangle() {
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
     
-    
+    return shaderProgram;
+}
+/// 画三角形
+void drawTriangle(GLFWwindow* window) {
+    unsigned int shaderProgram = createProgram(vertexShaderSource, fragmentShaderSource);
+    unsigned int shaderProgramGreen = createProgram(vertexShaderSource, fragmentShaderSourceGreen);
+    // MARK: - 创建顶点和缓冲对象
     // 三角形顶点
     float vertices[] = {
         -0.5f, -0.5f,0.0f,
         0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f,
+        -0.5f, 0.7f, 0.0f,
+        0.5f, 0.7f, 0.0f,
         0.0f, 0.5f, 0.0f
     };
     // VAO对象 VBO对象
-    unsigned int VAO;
-    glGenVertexArrays(1, & VAO);
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
-    unsigned int VBO;
     // 绑定缓冲
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     // 复制顶点数据
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 激活
-    glUseProgram(shaderProgram);
-    glBindVertexArray(VAO);
     // 解析顶点数据
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // 绘制物体
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindVertexArray(0);
+    // 循环
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glUseProgram(shaderProgramGreen);
+        glDrawArrays(GL_TRIANGLES, 3, 6);
+        // glBindVertexArray(0); // no need to unbind it every time
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+}
+/// 画两个三角形（四边形）
+void drawTwoTrians(GLFWwindow *window) {
+    unsigned int shaderProgram = createProgram(vertexShaderSource, fragmentShaderSource);
+    // MARK: - 创建顶点和缓冲对象
+    // 四角形顶点
+    float vertices[] = {
+        0.5f, 0.5f, 0.0f,   // 右上角
+        0.5f, -0.5f, 0.0f,  // 右下角
+        -0.5f, -0.5f, 0.0f, // 左下角
+        -0.5f, 0.5f, 0.0f   // 左上角
+    };
+    // 顶点绘制顺序
+    unsigned int indices[] = { // 注意索引从0开始!
+        0, 1, 3, // 第一个三角形
+        1, 2, 3  // 第二个三角形
+    };
+    // VAO对象 VBO对象 EBO对象
+    unsigned int VAO, VBO, EBO;
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, & VAO);
+    glGenBuffers(1, &VBO);
+    glBindVertexArray(VAO);
+    // 绑定缓冲 复制顶点数据
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // 解析顶点数据
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // 循环
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        // 线框模式
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        // glBindVertexArray(0); // no need to unbind it every time
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 }
